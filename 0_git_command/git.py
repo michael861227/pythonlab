bd2fc6411c539839e82f9276cffd9f3b336b8ef9
michael861227 <michael861227@gmail.com>
2019-03-16 15:29:08 +0800

    copy git command

---
+++ 0_git_command/git.py
@@ -0,0 +1,12 @@
+print("github: ")
+
+print("git status")
+#觀測目前狀態
+print("git add .")
+#紀錄更改項目
+print("git status")
+print("git commit -m  blablabla ")
+#將更改項目寫入歷史(github)
+print("git status")
+print("git push origin master")
+#上傳到github
